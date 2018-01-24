fe3e0f8b8e228783d07395881945331976cd7e22
hujian <hujiankkkk2012@126.com>
2018-01-24 15:56:48 +0800

    注释1

--- src/main/java/com/jxnc/hujian/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/jxnc/hujian/eureka_server/EurekaServerApplication.java
@@ -3,7 +3,11 @@ package com.jxnc.hujian.eureka_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/**
+ * 测试注释
+ * @author Administrator
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
