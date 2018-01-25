73f44d820b481c4ec02c390b1455f0af1096ac64
hujian <hujiankkkk2012@126.com>
2018-01-25 11:09:41 +0800

    add new

---
+++ src/main/java/com/jxnc/hujian/eureka_server/web/WebTest.java
@@ -0,0 +1,15 @@
+package com.jxnc.hujian.eureka_server.web;
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
+public class WebTest {
+
+	public static void main(String[] args) {
+		System.out.println("just test");
+
+	}
+
+}
