32a40182ceb1285d4344f37fdc6392768785b0d8
mainame123 <mainame123@126.com>
2018-01-25 11:01:53 +0800

    upd by page

--- src/main/java/com/jxnc/hujian/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/jxnc/hujian/eureka_server/EurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * @author Administrator
- *
+ *  页面上修改的 
  */
 @SpringBootApplication
 @EnableEurekaServer
